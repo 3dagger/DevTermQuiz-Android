c61d544faa3210be1d383bff5023f63c09108211
수현 <leeam@suhyeon-ui-MacBookPro-2.local>
2021-04-02 16:45:21 +0900

    #viewModel 작업 중

--- app/src/main/java/com/dagger/devtermquiz/model/QuizList.kt
+++
@@ -1,8 +0,0 @@
-package com.dagger.devtermquiz.model
-
-import com.google.gson.annotations.SerializedName
-
-data class QuizList(
-        @SerializedName("key") val key: String?,
-        @SerializedName("value") val value: String?
-)
