e30888a74b0202217c90cb4311be078bddcf4ee1
수현 <leeam@suhyeon-ui-MacBookPro-2.local>
2021-04-09 09:11:04 +0900

    #ViewPager

--- app/src/main/java/com/dagger/devtermquiz/view/main/MainActivity.kt
+++ app/src/main/java/com/dagger/devtermquiz/view/main/MainActivity.kt
@@ -1,30 +1,20 @@
 package com.dagger.devtermquiz.view.main
 
-import android.os.Handler
+import android.graphics.Color
 import androidx.lifecycle.Observer
 import androidx.viewpager2.widget.ViewPager2
-import com.dagger.devtermquiz.BR
 import com.dagger.devtermquiz.R
 import com.dagger.devtermquiz.adapter.PagerRecyclerAdapter
 import com.dagger.devtermquiz.base.BaseActivity
-import com.dagger.devtermquiz.base.BaseRecyclerView
 import com.dagger.devtermquiz.databinding.ActivityMainBinding
-import com.dagger.devtermquiz.databinding.QuestionItemBinding
-import com.dagger.devtermquiz.ext.show
-import com.dagger.devtermquiz.model.django.quiz.SingleQuizExample
-import com.dagger.devtermquiz.model.django.quiz.SingleQuizResults
 import com.dagger.devtermquiz.view.main.model.MainViewModel
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import com.orhanobut.logger.Logger
-import com.victor.loading.rotate.RotateLoading
+import com.taishi.flipprogressdialog.FlipProgressDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.loading_view.*
 import kotlinx.android.synthetic.main.question_item.*
 import org.koin.android.ext.android.inject
+
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), MainNavigator.View {
 
@@ -32,16 +22,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), MainNav
     override val viewModel: MainViewModel by inject()
 
 
+
     override fun initView() {
         viewModel.setNavigator(this)
 
+        val imageList: MutableList<Int> = ArrayList()
+        imageList.add(R.drawable.ic_thumb_up_black_24dp)
+        imageList.add(R.drawable.ic_thumb_down_black_24dp)
 
-        rotateloading.show()
-        rotateloading.start()
+        val dim = FlipProgressDialog()
+        dim.setImageList(imageList)
+        dim.setOrientation("rotationY")
 
-        Handler().postDelayed({
-            rotateloading.stop()
-        }, 10000)
+        dim.setDimAmount(0.8f)
+        dim.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        dim.show(fragmentManager, "")
+
+//        dim.dismiss()
+
 
         viewDataBinding.run {
             lifecycleOwner = this@MainActivity
@@ -63,7 +61,7 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), MainNav
             viewPager.apply {
                 adapter = PagerRecyclerAdapter(result = it.results)
                 orientation = ViewPager2.ORIENTATION_HORIZONTAL
-                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
                     override fun onPageSelected(position: Int) {
                         super.onPageSelected(position)
                         Logger.d("Page $position")
